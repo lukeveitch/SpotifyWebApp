d181f6397906cefbc86c279cd50455f25f17d99b
Luke Veitch <63455910+lukeveitch@users.noreply.github.com>
2022-07-27 18:06:56 +0100

    Seperates classes and update of SpotifyArt file

---
+++ ImageGenerator.py
@@ -0,0 +1,19 @@
+import os
+import glob
+
+class ImageGenerator(object):
+    path_to_script_dir = None
+    
+    def __init__(self, path_to_script_dir, processing_script):
+        self.path_to_script_dir = path_to_script_dir.removesuffix('\\').removesuffix('/')       
+        self.processing_script = processing_script
+        
+    def makeArt(self): # !!!!!!!!!! add a more meaningful name to the file that is generated
+        #run cli command
+        stream = os.popen(f'java -jar {self.path_to_script_dir}\\processing-py.jar {self.path_to_script_dir}\\{self.processing_script}')
+        output = stream.read()
+
+        #get most recent file
+        list_of_files = glob.glob(f'{self.path_to_script_dir}\\Examples\\*')
+        latest_file = max(list_of_files, key=os.path.getctime)
+        return latest_file
