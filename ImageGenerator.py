44633fc3b7c48c347e0636bc68a70432e23179fd
Luke Veitch <63455910+lukeveitch@users.noreply.github.com>
2022-08-02 14:38:00 +0100

    changed the backslashes in ImageGenerator

--- ImageGenerator.py
+++ ImageGenerator.py
@@ -7,12 +7,14 @@ class ImageGenerator(object):
     def __init__(self, path_to_script_dir, processing_script):
         self.path_to_script_dir = path_to_script_dir.removesuffix('\\').removesuffix('/')       
         self.processing_script = processing_script
+
+        print(self.path_to_script_dir)
         
     def makeArt(self): # !!!!!!!!!! add a more meaningful name to the file that is generated
         #run cli command
-        stream = os.popen(f'java -jar {self.path_to_script_dir}\\processing-py.jar {self.path_to_script_dir}\\{self.processing_script}\n')
+        stream = os.popen(f'java -jar {self.path_to_script_dir}\\processing-py.jar {self.path_to_script_dir}\\{self.processing_script}')
         output = stream.read()
         #get most recent file
-        list_of_files = glob.glob(f'{self.path_to_script_dir}\\Examples\\*')
+        list_of_files = glob.glob(f'{self.path_to_script_dir}\\Examples\\*') 
         latest_file = max(list_of_files, key=os.path.getctime)
         return latest_file
