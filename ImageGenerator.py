c1ad9c55413561f1558c495759cfb974a93734b7
Luke Veitch <63455910+lukeveitch@users.noreply.github.com>
2022-08-08 21:25:02 +0100

    file path changes in ImageGenerator()

--- ImageGenerator.py
+++ ImageGenerator.py
@@ -11,10 +11,17 @@ class ImageGenerator(object):
         print(self.path_to_script_dir)
         
     def makeArt(self): # !!!!!!!!!! add a more meaningful name to the file that is generated
+
+        processing_cli_command = f'java -jar {self.path_to_script_dir}/processing-py.jar {self.path_to_script_dir}/{self.processing_script}'
+        print(processing_cli_command)
         #run cli command
-        stream = os.popen(f'java -jar {self.path_to_script_dir}\\processing-py.jar {self.path_to_script_dir}\\{self.processing_script}')
+        stream = os.popen(processing_cli_command)
         output = stream.read()
         #get most recent file
         list_of_files = glob.glob(f'{self.path_to_script_dir}\\Examples\\*') 
         latest_file = max(list_of_files, key=os.path.getctime)
         return latest_file
+
+dir_path = "/PietMondrian/"
+script_name = "PietMondrian.pyde"
+imageGenerator = ImageGenerator(dir_path, script_name)
--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 import flask
 from SpotifyClient import SpotifyClient, client_id, client_secret
 from ImageGenerator import ImageGenerator
+import sys
+sys.path.append('../')
+
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
-
 
 playlist_id = '5AAD6wso6ksiY8V7yu6Gr2'
 dir_path = "\\PietMondrian\\"
@@ -14,8 +16,8 @@ spotifyClient = SpotifyClient(client_id, client_secret)
 is_success, playlist_image, track_ids = spotifyClient.getPlaylistInfo(playlist_id)
 is_success, audio_features = spotifyClient.getFeatures(track_ids)
 imageGenerator = ImageGenerator(dir_path, script_name)
-image_art_url = imageGenerator.makeArt() #will want to pass in the audiofeatures eventually - use input() function in processing script, to then pass the inputs into the cli using os lib
-
+image_art_url = imageGenerator.makeArt() 
+#will want to pass in the audiofeatures eventually - use input() function in processing script, to then pass the inputs into the cli using os lib
 
 @app.route('/artImage', methods=['GET'])
 def api_all():
@@ -23,4 +25,3 @@ def api_all():
 
 if __name__ == "__main__":
     app.run()
-
