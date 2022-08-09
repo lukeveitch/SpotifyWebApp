b2b314766c434d433ba0a06bf91ce3657b02cc1e
Luke Veitch <63455910+lukeveitch@users.noreply.github.com>
2022-08-09 10:51:25 +0100

    fixed file path problem and working version of flask app

--- ImageGenerator.py
+++ ImageGenerator.py
@@ -1,21 +1,21 @@
 import os
 import glob
 
+    
 class ImageGenerator(object):
     path_to_script_dir = None
     
     def __init__(self, path_to_script_dir, processing_script):
-        self.path_to_script_dir = path_to_script_dir.removesuffix('\\').removesuffix('/')       
+        self.path_to_script_dir = os.getcwd() + path_to_script_dir.removesuffix('\\').removesuffix('/')       
         self.processing_script = processing_script
 
     def makeArt(self): # !!!!!!!!!! add a more meaningful name to the file that is generated
-        #run cli command
-        print(self.path_to_script_dir)
-        print(self.processing_script)
-
-        stream = os.popen(f'java -jar {self.path_to_script_dir}\\processing-py.jar {self.path_to_script_dir}\\{self.processing_script}\n')
+        command = f'java -jar {self.path_to_script_dir}\\processing-py.jar {self.path_to_script_dir}\\{self.processing_script}\n'
+        save_image_path = f'{self.path_to_script_dir}\\Examples\\*'
+        
+        stream = os.popen(command)
         output = stream.read()
-        #get most recent file
-        list_of_files = glob.glob(f'{self.path_to_script_dir}\\Examples\\*')
+
+        list_of_files = glob.glob(save_image_path)
         latest_file = max(list_of_files, key=os.path.getctime)
         return latest_file
--- api.py
+++ api.py
@@ -12,24 +12,16 @@ playlist_id= '73KvoZbKdQSNonmuvKrUII'
 dir_path = "\\PietMondrian\\"
 script_name = "PietMondrian.pyde"
 
-cwd = os.getcwd()
-
-print(cwd)
-print(dir_path)
-print(cwd + dir_path)
-print(cwd + dir_path + script_name)
-print(cwd + dir_path + "Examples\\")
-
-# spotifyClient = SpotifyClient(client_id, client_secret)
-# is_success, playlist_image, track_ids = spotifyClient.getPlaylistInfo(playlist_id)
-# is_success, audio_features = spotifyClient.getFeatures(track_ids)
-# imageGenerator = ImageGenerator(dir_path, script_name)
-# image_art_url = imageGenerator.makeArt() #will want to pass in the audiofeatures eventually - use input() function in processing script, to then pass the inputs into the cli using os lib
+spotifyClient = SpotifyClient(client_id, client_secret)
+is_success, playlist_image, track_ids = spotifyClient.getPlaylistInfo(playlist_id)
+is_success, audio_features = spotifyClient.getFeatures(track_ids)
+imageGenerator = ImageGenerator(dir_path, script_name)
+image_art_url = imageGenerator.makeArt() #will want to pass in the audiofeatures eventually - use input() function in processing script, to then pass the inputs into the cli using os lib
 
 
-# @app.route('/artImage', methods=['GET'])
-# def api_all():
-#     return image_art_url
+@app.route('/artImage', methods=['GET'])
+def api_all():
+    return image_art_url
 
-# app.run()
+app.run()
 
